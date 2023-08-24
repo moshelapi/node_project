3f176a97d02dc91fc6bc165d2ea0e430c1364d3e
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-08-24 14:00:10 +0300

    npm i cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express'
 import { router } from './api/router/router.product.js';
 import routerUsers from './users/router/router.users.js';
 import morgan from 'morgan';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
-app.use('/api',router)
+app.use(cors())
+app.use('/api/product',router)
 app.use(morgan('tiny'));
-app.use('/users',routerUsers)
+app.use('/api/users',routerUsers)
 
 
 
